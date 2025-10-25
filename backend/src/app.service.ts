6c6dac06168dda4c2e696526328eda5565c3a350
Kleverson Eller <kleverson.a16@gmail.com>
2025-10-25 20:19:22 -0300

    feat: :sparkles: Initialize and configuration Swagger
    Instalando e cunfigurando a documentação da API com Swagger

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Documentação Swagger disponível em: <a href="http://localhost:3001/api/docs" rel="noopener noreferrer">http://localhost:3001/api/docs</a>`;
   }
 }

4a7b5df9cbfd3c7a2589873fea421d2ebe16858d
Kleverson Eller <kleverson.a16@gmail.com>
2025-10-25 18:41:46 -0300

    feat: :sparkles: Initialize and configuration Prisma
    Instalando e configurando o prisma com o docker do banco de dados e Nestjs

---
+++ backend/prisma/migrations/20251025170557_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "Aluno" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Aluno_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Curso" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+
+    CONSTRAINT "Curso_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Matricula" (
+    "id" SERIAL NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    "cursoId" INTEGER NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Matricula_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Aluno_email_key" ON "Aluno"("email");
+
+-- AddForeignKey
+ALTER TABLE "Matricula" ADD CONSTRAINT "Matricula_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Matricula" ADD CONSTRAINT "Matricula_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

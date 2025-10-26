bb88e49a7110cb7f14d76e2128894cf6b2c5fdb1
Kleverson Eller <kleverson.a16@gmail.com>
2025-10-26 16:46:53 -0300

    feat: :sparkles: Implement logs for Matriculas
    Adiciona registro de logs para a entidade Matriculas.

--- backend/src/matriculas/matriculas.module.ts
+++ backend/src/matriculas/matriculas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatriculasService } from './matriculas.service';
 import { MatriculasController } from './matriculas.controller';
+import { PrismaModule } from 'prisma/prisma.module';
+import { LogsModule } from 'src/logs/logs.module';
 
 @Module({
+  imports: [PrismaModule, LogsModule],
   controllers: [MatriculasController],
   providers: [MatriculasService],
 })
--- backend/src/matriculas/matriculas.service.ts
+++ backend/src/matriculas/matriculas.service.ts
@@ -2,15 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { CreateMatriculaDto } from './dto/create-matricula.dto';
 import { UpdateMatriculaDto } from './dto/update-matricula.dto';
 import { PrismaService } from '../../prisma/prisma.service';
+import { LogsService } from 'src/logs/logs.service';
 
 @Injectable()
 export class MatriculasService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private logsService: LogsService,
+  ) {}
 
-  create(data: CreateMatriculaDto) {
-    return this.prisma.matricula.create({
+  async create(data: CreateMatriculaDto) {
+    const matricula = await this.prisma.matricula.create({
       data,
     });
+
+    await this.logsService.create(
+      `Matrícula criada: ID ${matricula.id}, AlunoID: ${matricula.alunoId}, CursoID: ${matricula.cursoId}`,
+      'MatriculasService.create',
+    );
+
+    return matricula;
   }
 
   findAll() {
@@ -23,16 +34,30 @@ export class MatriculasService {
     });
   }
 
-  update(id: number, data: UpdateMatriculaDto) {
-    return this.prisma.matricula.update({
+  async update(id: number, data: UpdateMatriculaDto) {
+    const matriculaUpdate = await this.prisma.matricula.update({
       where: { id },
       data,
     });
+
+    await this.logsService.create(
+      `Matrícula atualizada: ID ${matriculaUpdate.id}, AlunoID: ${matriculaUpdate.alunoId}, CursoID: ${matriculaUpdate.cursoId}`,
+      'MatriculasService.update',
+    );
+
+    return matriculaUpdate;
   }
 
-  remove(id: number) {
-    return this.prisma.matricula.delete({
+  async remove(id: number) {
+    const matricula = await this.prisma.matricula.delete({
       where: { id },
     });
+
+    await this.logsService.create(
+      `Matrícula removida: ID ${matricula.id}, AlunoID: ${matricula.alunoId}, CursoID: ${matricula.cursoId}`,
+      'MatriculasService.remove',
+    );
+
+    return matricula;
   }
 }

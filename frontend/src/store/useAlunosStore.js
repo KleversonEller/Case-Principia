11665c19a553628b322b704203b5659f33968073
Kleverson Eller <kleverson.a16@gmail.com>
2025-10-30 20:47:34 -0300

    revert: :rewind: Deleted items
    Excluindo itens que não vai dar tempo de implementar

--- frontend/src/store/useAlunosStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useAlunosStore = create((set) => ({
-  alunos: [],
-  setAlunos: (alunos) => set({ alunos }),
-  addAluno: (aluno) => set((state) => ({ alunos: [...state.alunos, aluno] })),
-}));

98e9139e25f716c53658e5f1f4189cc683586f17
Kleverson Eller <kleverson.a16@gmail.com>
2025-10-30 18:11:53 -0300

    chore: :wrench: Configuração inicial
    Limpeza das configurações padão do react + vite e setup inicial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-import AlunosPage from './pages/alunosPage'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AlunosPage from "./pages/AlunosPage";
+import './index.css';
 
 export default function App() {
-  return <AlunosPage />
+  return (
+    <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<AlunosPage />} />
+          </Routes>
+        </main>
+    </Router>
+  );
 }
